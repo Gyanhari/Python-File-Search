ab9d62588aecd2e74d7fd632cfb2f29c6f593cb8
Gyan Hari Dahal <dgyanhari@gmail.com>
2024-03-11 22:41:23 +0545

    Added Index

--- index.py
+++ index.py
@@ -1,8 +1,11 @@
 import os 
 import os.path as path
 
+def try1():
+	print(__name__)
 
+try1()
 
 if __name__ == "__main__" :
-	main()
-	print("\nThank you for using our system");
+	# main()
+	print("\nThank you for using our system")
