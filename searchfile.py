8a7ad88961c437ee658d37bdd92cac3cb6deb5f3
Yubaraj Poudel <poudel.yubaraj42@gmail.com>
2024-03-18 23:03:58 +0545

    walk part

--- searchfile.py
+++ searchfile.py
@@ -1,6 +1,19 @@
-from dependency import os,path,re
+from dependency import os, path, re
 
 def searchfile(file_name):
-    entered_file=file_name
-     
+    file_existence("sample",file_name)
+
+
+
+
+def file_existence(drive,file_name):
+    matched_files = [] 
+    for root, dirs, files in os.walk(drive):
+        for file in files:
+            if file == file_name:
+                temp=fun_name(root,file)
+                matched_files.append(temp)
+               
+           
+
 
