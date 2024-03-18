85dd8b21efb99febd5c111b9091fb52d124407b1
Gyan Hari Dahal <dgyanhari@gmail.com>
2024-03-18 21:43:43 +0545

    Added a method to get Drives in windows and updated readme.md

--- searchfile.py
+++ searchfile.py
@@ -1,4 +1,9 @@
 from dependency import os,path,re
+import win32api
+
+drives = win32api.GetLogicalDriveStrings()
+drives = drives.split('\000')[:-1]
+print(drives)
 
 def searchfile(file_name):
     pass
--- searchfolder.py
+++ searchfolder.py
@@ -1,4 +1,9 @@
 from dependency import os,path,re
+import win32api
+
+drives = win32api.GetLogicalDriveStrings()
+drives = drives.split('\000')[:-1]
+print(drives)
 
 def searchfolder(folder_name):
     pass
